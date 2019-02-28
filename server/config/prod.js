72d9c7f41874e83aace9745142e0302508d1977a
goginim <david.ingook@gmail.com>
2019-02-28 12:57:31 -0500

    deploy

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,5 @@
 //prod.js production keys here!!
 module.exports = {
-  mongooseURI: process.env.MONGOOSE_MEMO_URI,
+  mongooseURI: process.env.MONGOOSE_URI,
   sessionSecret: process.env.SESSION_SECRET
 };
